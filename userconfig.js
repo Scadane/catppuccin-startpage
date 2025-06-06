e9a1f8cfa1ddf5dcebb6799b7c0b5a4b25555068
ScaDane <74491344+Scadane@users.noreply.github.com>
2025-06-06 16:35:43 +0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -217,7 +217,7 @@ const default_configuration = {
     },
     {
       name: "chi ll",
-      background_url: "src/img/banners/banner_14.gif",
+      background_url: "src/img/banners/banner_15.gif",
       categories: [
         {
           name: "social media",
